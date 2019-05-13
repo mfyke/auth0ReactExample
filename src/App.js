83a50c670a6b60a2dacc155867c5588eba417173
matt fyke <mfyke@live.com>
2019-05-13 13:41:05 -0700

    switched back to BrowserRouter to be used on local host

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Secret from './components/Secret';
 import Error from './components/Error';
 import Callback from './components/Callback';
 import Auth from './Auth';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const auth = new Auth();
 let username = auth.getProfile().given_name || "User";
@@ -20,14 +20,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HashRouter basename="/">
+        <BrowserRouter>
           <Switch>
             <Route path="/secret" render={()=> this.state.auth.isAuthenticated() ? <Secret auth={this.state.auth} name={this.state.name} /> : <Error/>} />
             <Route path="/" render={() => <Main auth={this.state.auth} name={this.state.name} />} exact />
             <Route path="/callback" component={Callback} />
             <Route component={Error} default />  
           </Switch>
-        </HashRouter>
+        </BrowserRouter>
       </div>
     );
   }
